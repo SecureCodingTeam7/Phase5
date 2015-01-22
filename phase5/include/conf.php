9bdacbd13ea503fa83788531256915ce77f39ee9
root <root@samurai-wtf.localhost>
2015-01-22 13:29:59 +0100

    changed db password

--- phase5/include/conf.php
+++ phase5/include/conf.php
@@ -4,5 +4,5 @@
 
 	define( "DB_NAME", "mysql:host=localhost;dbname=mybank" );
 	define( "DB_USER", "mybankRoot" );
-	define( "DB_PASS", "74VKxSYk8B6g" );
+	define( "DB_PASS", "74VKrexSYk8B6g" );
 ?>
